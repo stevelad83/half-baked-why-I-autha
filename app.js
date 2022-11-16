ec7b7ca7b8e7815492acf0336e4adefa7e323fac
Steven Bryant <stevelad_83@yahoo.com>
2022-11-16 15:21:14 -0500

    added signup functionality

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+import { signupUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const email = data.get('email');
+    const user = await signupUser(email, data.get('password'));
+
+    // if (user) {
+    //     location.replace('/other-page');
+    // }
+});
 
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
--- fetch-utils.js
+++ fetch-utils.js
@@ -9,11 +9,21 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
 
-export async function signInUser(email, password) {}
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+    return response.user;
+}
 
-export async function checkAuth() {}
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('../');
+}
 
 export async function redirectIfLoggedIn() {}
 
