0ad2712e25e1cb44a1627f211e620be69b06d383
Steven Bryant <stevelad_83@yahoo.com>
2022-11-16 16:01:26 -0500

    added checkAuth to signInUser

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { redirectIfLoggedIn, signInUser, signupUser } from './fetch-utils.js';
+import { checkAuth, redirectIfLoggedIn, signInUser, signupUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
 
@@ -19,12 +19,15 @@ signUpForm.addEventListener('submit', async (e) => {
 
 signInForm.addEventListener('submit', async (e) => {
     e.preventDefault();
+
     const data = new FormData(signInForm);
     const email = data.get('email');
     const user = await signInUser(email, data.get('password'));
 
     if (user) {
         location.replace('../other-page');
+    } else {
+        checkAuth();
     }
 });
 // Wire up sign in and sign up forms to supabase
